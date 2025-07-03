f65308cf2da91ad0a60649642cb92152d7b45713
Jake <waters.1909@gmail.com>
2025-07-02 22:19:18 -0600

    updating

--- set_bible_chapter.py
+++ set_bible_chapter.py
@@ -1,9 +1,13 @@
+#!/usr/bin/python3
+
 import os
 import json
 import random
 import requests
 import datetime
 from bs4 import BeautifulSoup
+from plyer import notification
+
 '''
 bibles = requests.get('https://api.scripture.api.bible/v1/bibles', headers=headers).json()['data']
 for bible in bibles:
@@ -14,8 +18,7 @@ for bible in bibles:
 # curl -X GET "https://api.scripture.api.bible/v1/bibles/06125adad2d5898a-01/books" -H  "accept: application/json" -H  "api-key: $API_BIBLE_KEY" | jq . > books.json
 
 file_path = "/home/jacwater/.cache/bible/daily.json"
-with open(file_path, 'r') as file:
-    current = json.load(file)
+current = json.load(open(file_path))
 
 output = {
     "refresh_ts": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
@@ -24,7 +27,7 @@ output = {
 
 time_since_last_refresh = (datetime.datetime.now() - datetime.datetime.strptime(current['refresh_ts'], "%Y-%m-%d %H:%M:%S")).days
 
-if time_since_last_refresh >= 1: 
+if current['date'] != output['date']:
     headers = {
         'content-type': 'application/json',
         'api-key': os.getenv('API_BIBLE_KEY'),
@@ -62,12 +65,22 @@ if time_since_last_refresh >= 1:
             chapter_text = '%s %s' % (chapter_text, text.replace(str(i+1), verse_identifier))
     output['text'] = chapter_text
 
-    # create json file
-    with open(file_path, 'w') as f:
-        # Write the data to the file
-        json.dump(output, f, indent=4)
-
-    print('Updating Bible Chapter...')
-    print(output)
+    # write new output to json and set message
+    json.dump(output, open(file_path, 'w'), indent=4)
+    bible_message = 'INFO: Updating Daily Bible Chapter - %s' % (output['chapter'])
 else:
-    print('Bible Chapter is up-to-date')
+    # update timestamp of last poll and write update to json file
+    current['refresh_ts'] = output['refresh_ts']
+    current['date'] = output['date']
+    json.dump(current, open(file_path, 'w'), indent=4)
+
+    # set message
+    bible_message = 'INFO: Bible Chapter is already up-to-date - %s' % (current['chapter'])
+
+# send message to desktop
+notification.notify(
+    title='Bible Scripture Agent',
+    message=bible_message,
+    app_name='BibleScriptureAgent',
+    timeout=360
+)
